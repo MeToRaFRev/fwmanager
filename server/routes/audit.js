b29f32d8fc3430cb7fde1973aaa54bd0946a65a5
MeToRaFRev <MeToRaFRev@gmail.com>
2025-03-30 18:39:31 +0300

    fixes

--- server/routes/audit.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auditController = require('../controllers/auditController');
-const { authenticateToken } = require('../middlewares/authMiddleware');
-
-router.get('/audit', authenticateToken, auditController.getAuditLog);
-
-module.exports = router;
